f83412b9ff33befe18acffd97f4b0365e92ac960
jasharicoco <jashari.egzon@hotmail.com>
2024-10-17 23:11:10 +0200

    Added car class with a few methods.

---
+++ OOP_Ovning_ConstructorsFieldsProperties/Car.cs
@@ -0,0 +1,44 @@
+﻿namespace OOP_Ovning_ConstructorsFieldsProperties
+{
+    internal class Car
+    {
+        private static int _instanceCount = 0;
+        private static List<Car> _instances = new List<Car>();
+        
+        private string _brand;
+        private string _model;
+        private int _year;
+
+        public static int InstanceCount
+        {
+            get { return _instanceCount; }
+        }
+
+        public Car(string brand, string model, int year)
+        {
+            _brand = brand;
+            _model = model;
+            _year = year;
+            _instanceCount++;
+            _instances.Add(this);
+        }
+
+        public void Present()
+        {
+            Console.WriteLine($"This car is a {_brand} {_model} from {_year}");
+        }
+
+        public static double AverageYear()
+        {
+            if (_instances.Count == 0) return 0; // In case of no cars
+
+            double total = 0;
+            foreach (var car in _instances)
+            {
+                total += car._year; // Add each cars year in the total sum
+            }
+
+            return (total / _instances.Count); // Calculate average
+        }
+    }
+}
--- OOP_Ovning_ConstructorsFieldsProperties/Program.cs
+++ OOP_Ovning_ConstructorsFieldsProperties/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             // Book
+
+            Console.WriteLine("Book");
+
             Book book1 = new Book("A Fish", "Kulusevski", 1204);
             Book book2 = new Book("Stories of magnificent places", "Charles Bukowski", 185);
             Book book3 = new Book("Understanding women", "The Wisest Man", 1982132521);
@@ -15,6 +18,8 @@
 
             // Bank Account
 
+            Console.WriteLine("\nBank Account");
+
             BankAccount account1 = new BankAccount("252525");
 
             account1.Deposit(200);
@@ -24,6 +29,7 @@
             Console.WriteLine($"Account overdrawn? {account1.IsOverdrawn}");
 
             // Circle
+            Console.WriteLine("\nCircle");
 
             Circle circle1 = new Circle(5);
 
@@ -32,12 +38,14 @@
             Console.WriteLine($"{circle1.Area}");
 
             // Employee
+            Console.WriteLine("\nEmployee");
 
             Employee employee1 = new Employee("Frank", "Boss", 500);
             employee1.Present();
             Console.WriteLine($"{employee1.YearlyIncome()} dollars a year.");
 
             // Temperature
+            Console.WriteLine("\nTemperature");
 
             Temperature temp1 = new Temperature(50);
             temp1.ShowTemp();
@@ -45,7 +53,14 @@
             temp1.ShowTemp();
 
             // Car
+            Console.WriteLine("\nCar");
 
+            Car car1 = new Car("Toyota", "X-Fly", 2001);
+            Car car2 = new Car("Honda", "Civic", 1986);
+            Car car3 = new Car("FillipinoDelux", "Oskarshamn", 2016);
+
+            Console.WriteLine($"Number of Cars in our catalogue: {Car.InstanceCount}");
+            Console.WriteLine($"Average year of production for all our cars is {Car.AverageYear()}");
         }
     }
 }
